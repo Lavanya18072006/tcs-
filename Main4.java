bb14e8a5b56837315ddfc5f1fd0f697042f49916
Lavanya R <lavanya18072006@gmail.com>
2026-07-01 09:36:28 +0530

    Add files via upload

---
+++ Main4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main4 {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a num:- ");
+        int num = sc.nextInt();
+        int max = 10; 
+        while(num > 0){
+            max = num % 10 < max ? num % 10 : max;
+            num = num/10;
+        }
+        System.out.println(max);
+    }
+}
