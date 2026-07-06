05adf7011a82ca465bcf1d3e06578ee5d5d494e2
Lavanya R <lavanya18072006@gmail.com>
2026-07-06 11:46:49 +0530

    Add files via upload

---
+++ Cryptographic.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Cryptographic {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n <= 1) {
+            System.out.println("Untrusted Node");
+            return;
+        }
+
+        boolean isPrime = true;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            System.out.println("Trusted Node");
+        } else {
+            System.out.println("Untrusted Node");
+        }
+    }
+}
